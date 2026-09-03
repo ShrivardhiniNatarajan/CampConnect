085aa8c1e8477bf156f98adff45f4d866b7f72fc
ShrivardhiniNatarajan <shrivardhininatarajan@gmail.com>
2026-09-03 16:48:34 +0530

    feat: add frontend foundation

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-gray-900">
+        CampConnect
+      </h1>
+    </div>
+  )
+}
+
+export default App
